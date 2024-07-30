33473ea35f6d99bb77b579b08135a148b1f22f48
parksangryong <parksangryong@Mac-mini-2.local>
2024-07-30 17:27:11 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
   runApp(MyApp());
